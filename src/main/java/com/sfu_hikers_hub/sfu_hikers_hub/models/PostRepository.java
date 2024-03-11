4f849d09499cc1fe8367bd14312ba13c339a752c
Tony Chen <112990072+TonyDEChen@users.noreply.github.com>
2024-03-10 22:36:05 -0700

    fix typo

--- src/main/java/com/sfu_hikers_hub/sfu_hikers_hub/models/PostRepository.java
+++ src/main/java/com/sfu_hikers_hub/sfu_hikers_hub/models/PostRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
-    List<Post> findByPid();
+    List<Post> findByPid(int pid);
 }
