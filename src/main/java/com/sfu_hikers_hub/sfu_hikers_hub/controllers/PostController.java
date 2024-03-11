334ef7ad3b04aebe9cbec3bef10797fe840c9534
Tony Chen <112990072+TonyDEChen@users.noreply.github.com>
2024-03-10 23:10:23 -0700

    forum main page prototype

--- src/main/java/com/sfu_hikers_hub/sfu_hikers_hub/controllers/PostController.java
+++ src/main/java/com/sfu_hikers_hub/sfu_hikers_hub/controllers/PostController.java
@@ -1,8 +1,13 @@
 package com.sfu_hikers_hub.sfu_hikers_hub.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.sfu_hikers_hub.sfu_hikers_hub.models.Post;
 import com.sfu_hikers_hub.sfu_hikers_hub.models.PostRepository;
 
 @Controller
@@ -11,4 +16,11 @@ public class PostController {
     @Autowired
     private PostRepository postRepo;
     
+    @GetMapping("/posts/view")
+    public String getAllPosts(Model model){
+        System.out.println("Getting all posts");
+        List<Post> posts = postRepo.findAll();
+        model.addAttribute("ps", posts);
+        return "posts/forumPage";
+    }
 }
