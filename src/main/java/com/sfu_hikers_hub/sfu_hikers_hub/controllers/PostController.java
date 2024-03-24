2f30f5fef3e7d1d24e34f98e80b968fd5ff3f075
26-Lav <lavikasingh98@gmail.com>
2024-03-23 17:06:58 -0700

    Authenticating logged in users to post

--- src/main/java/com/sfu_hikers_hub/sfu_hikers_hub/controllers/PostController.java
+++ src/main/java/com/sfu_hikers_hub/sfu_hikers_hub/controllers/PostController.java
@@ -9,12 +9,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import com.sfu_hikers_hub.sfu_hikers_hub.models.User;
 import com.sfu_hikers_hub.sfu_hikers_hub.models.Post;
 import com.sfu_hikers_hub.sfu_hikers_hub.models.PostRepository;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class PostController {
@@ -33,6 +34,20 @@ public class PostController {
     @PostMapping("/posts/add")
     public String addPost(@RequestParam Map<String, String> newpost, HttpServletResponse response,
             HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            // This means no session was found
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN); // Set the response status to 403 Forbidden
+            return "redirect:/login"; // Redirect the user to the login page
+        }
+        User user = (User) session.getAttribute("session_user");
+        if (user == null) {
+            // This means no user is found in session, which implies the user is not logged
+            // in
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN); // Set the response status to 403 Forbidden
+            return "redirect:/login"; // Redirect the user to the login page
+        }
         System.out.println("Adding post");
         try {
             String op = "temp";
