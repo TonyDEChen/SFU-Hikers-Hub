2e34b21e97ce983ba6d21933e8144089d1e16af1
intelbentium <csim296@gmail.com>
2024-03-25 23:54:03 -0700

    bug fix

--- src/main/java/com/sfu_hikers_hub/sfu_hikers_hub/controllers/EventController.java
+++ src/main/java/com/sfu_hikers_hub/sfu_hikers_hub/controllers/EventController.java
@@ -14,9 +14,12 @@ import com.sfu_hikers_hub.sfu_hikers_hub.models.Event;
 import com.sfu_hikers_hub.sfu_hikers_hub.models.EventRepository;
 import com.sfu_hikers_hub.sfu_hikers_hub.models.Post;
 import com.sfu_hikers_hub.sfu_hikers_hub.models.User;
+import com.sfu_hikers_hub.sfu_hikers_hub.models.UserRepository;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+
+import java.util.ArrayList;
 import java.util.Collections;
 
 
@@ -25,6 +28,7 @@ public class EventController {
 
     @Autowired
     private EventRepository eventRepo;
+    private UserRepository userRepo;
 
     @GetMapping("/events/view")
     public String getAllEvents(Model model, HttpSession session){
@@ -85,8 +89,18 @@ public class EventController {
             if(user == null) return "redirect:/login";
             Event event = eventRepo.findByEid(eid);
             if(event == null) return "events/error";
+
+            List<User> attendeeList = new ArrayList<>();
+            List<Integer> attendees = event.getAttendees();
+            for(int i = 0; i < attendees.size(); i++)
+            {
+                attendeeList.add(userRepo.findByUid(attendees.get(i)));
+            }
+
             model.addAttribute("event", event);
             model.addAttribute("user", user);
+            model.addAttribute("list", attendeeList);
+            
 
             List<Integer> usersInEvent = event.getAttendees();
             for(int i = 0; i < usersInEvent.size(); i++)
