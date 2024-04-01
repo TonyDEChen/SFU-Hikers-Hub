b477f7929f83f14269c14e9de222d324b431486c
intelbentium <csim296@gmail.com>
2024-03-31 22:45:12 -0700

    API setup + gitignore api keys
    - API keys stored in local .env file and called upon
    - will be fed to render via enviornment variables

---
+++ src/main/java/com/sfu_hikers_hub/sfu_hikers_hub/models/WeatherService.java
@@ -0,0 +1,12 @@
+package com.sfu_hikers_hub.sfu_hikers_hub.models;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+
+    //load this via the local file
+    private String apiKey;
+    
+}
