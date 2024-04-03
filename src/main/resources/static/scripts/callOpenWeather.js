b0c95536cab7f5242fec65a4380ba260b0e7890e
intelbentium <csim296@gmail.com>
2024-04-02 23:52:54 -0700

    WIP (mapping to get key finally works)

--- src/main/resources/static/scripts/callOpenWeather.js
+++ src/main/resources/static/scripts/callOpenWeather.js
@@ -1,21 +1,43 @@
-var longitude = parseFloat(document.getElementById("lng").value);
-var latitude = parseFloat(document.getElementById("lat").value);
+window.addEventListener("load", (event) => {
+    console.log("page is fully loaded");
 
-var weatherKey = await fetch("/weather/getWeatherKey");
-var apiURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + weatherKey;
+    
+    var weatherKey;
 
-fetch(apiURL)
-    .then(response => {
-        if(!response.ok) {
-            throw new Error("api call");
-        }
-        return response.json();
+
+    fetch("/weather/getWeatherKey")
+        .then(response => response.text())
+        .then(data=> {
+            weatherKey = data;
+            return data;
         })
-        .then(data => {
-            console.log(data);
 
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
+        var longitude = parseFloat(document.getElementById("lng").value);
+        var latitude = parseFloat(document.getElementById("lat").value);
+    
+        var apiURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + weatherKey;
+        console.log(apiUrl);
+
+        fetch(apiURL)
+        .then(response => {
+            if(!response.ok) {
+                throw new Error("api call");
+            }
+            return response.json();
+            })
+            .then(data => {
+                console.log(data);
+
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+    });
+            
+    });
+    
+
+    
+
+
+
 
