acf18d4c9dbd3cf586c92b6339356a36a7570d68
intelbentium <csim296@gmail.com>
2024-04-03 21:34:24 -0700

    clean up and adjustement

--- src/main/resources/static/scripts/addEvent.js
+++ src/main/resources/static/scripts/addEvent.js
@@ -24,27 +24,5 @@ function initAutocomplete() {
         
     });
 }
-
-async function getWeatherData(latitude, longitude) 
-{
-    var weatherKey = await fetch("/weather/getWeatherKey");
-
-        var apiURL = 'https://history.openweathermap.org/data/2.5/aggregated/year?lat=' + latitude + '&lon=' + longitude + '&appid=' + weatherKey;
-        fetch(apiURL)
-            .then(response => {
-                if(!response.ok) {
-                    throw new Error("api call");
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            });
-}
-
 // Load the Google Places Autocomplete when the page is loaded
 google.maps.event.addDomListener(window, 'load', initAutocomplete);
